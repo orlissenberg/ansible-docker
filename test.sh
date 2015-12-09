96cfdeeec52a594d821f287cb79b9dfd58c59c0d
Onno Lissenberg <orlissenberg@gmail.com>
2015-12-09 01:58:20 +0100

    Debian 8 (Jessie) apt repository installation.

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-CID=$(sudo docker run -d -i orlissenberg/docker-sshd-wheezy);
+#CID=$(sudo docker run -d -i orlissenberg/docker-sshd-wheezy);
 #CID=$(sudo docker run -d -i orlissenberg/docker-sshd-centos6);
-IPADDRESS=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID});
+#IPADDRESS=$(sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID});
 
 # Create a hosts file.
 printf "[webservers]\n" > hosts
 
 # Install on localhost.
-#echo "localhost ansible_connection=local" >> hosts
+echo "localhost ansible_connection=local" >> hosts
 
 # Requires sshpass.
 # sudo apt-get install -y sshpass
 # sudo yum install -y sshpass
-printf "${IPADDRESS} ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=root\n" >> hosts
+# printf "${IPADDRESS} ansible_connection=ssh ansible_ssh_user=root ansible_ssh_pass=root\n" >> hosts
 
 # Need this because I'm running on Windows ...
 cp hosts /tmp/hosts
@@ -27,18 +27,18 @@ printf "host_key_checking = False\n" >> ansible.cfg
 ansible-playbook playbook.yml -i /tmp/hosts --extra-vars="docker_test=true"
 
 # Check if idempotent.
-ansible-playbook playbook.yml -i /tmp/hosts --extra-vars="docker_test=true"
+#ansible-playbook playbook.yml -i /tmp/hosts --extra-vars="docker_test=true"
 
 # Clean up.
 rm hosts; rm /tmp/hosts; rm ansible.cfg
 
 # Might need these commands for science!
-printf "sudo docker kill ${CID}\n"
-printf "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IPADDRESS\n"
+#printf "sudo docker kill ${CID}\n"
+#printf "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$IPADDRESS\n"
 
 # No longer need the container.
-sudo docker kill ${CID}
-sudo docker rm ${CID}
+#sudo docker kill ${CID}
+#sudo docker rm ${CID}
 
 # References:
 # http://stackoverflow.com/questions/17157721/getting-a-docker-containers-ip-address-from-the-host
